4374c7fc627df974cd67e33f32fcaa57d0401d2b
Adhika Guna <adhika@sirka.io>
2022-04-06 21:37:20 +0700

    hide private key

--- js/firebase.js
+++ js/firebase.js
@@ -9,15 +9,17 @@ import {
 } from "https://cdnjs.cloudflare.com/ajax/libs/firebase/9.5.0/firebase-database.min.js"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBaa2UTw3pZJqGf2XLolPinZZ-SeL7fctA",
-    authDomain: "auction-website-1cc67.firebaseapp.com",
-    databaseURL: "https://auction-website-1cc67-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "auction-website-1cc67",
-    storageBucket: "auction-website-1cc67.appspot.com",
-    messagingSenderId: "915777520945",
-    appId: "1:915777520945:web:0fdd3a7b50a5e0c72adc10",
-    measurementId: "G-E0QFPX769C"
+    apiKey: "YOUR_PRIVATE_KEY",
+    authDomain: "YOUR_DATA",
+    databaseURL: "YOUR_DATA",
+    projectId: "YOUR_DATA",
+    storageBucket: "YOUR_DATA",
+    messagingSenderId: "YOUR_DATA",
+    appId: "YOUR_DATA",
+    measurementId: "YOUR_DATA"
 };
+
+
 
 //initalize firebase
 const app = initializeApp(firebaseConfig)
